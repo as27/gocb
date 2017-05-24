138c63e52e25e18bd2d6500d092a3d0f460af76d
Andreas Schröpfer <andreas@apxp.info>
2017-05-24 11:48:10 +0200

    Read and write files ready

--- gocb/main.go
+++ gocb/main.go
@@ -34,9 +34,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Got an error while initializing: ", err)
 	}
+	log.Println("Write files")
 	err = writeFiles(files)
 	if err != nil {
 		log.Fatal("Got an error writing file", err)
+	}
+	log.Println("Read files")
+	fs, err := readFiles()
+	if err != nil {
+		log.Fatal("Error reading: ", err)
+	}
+	for _, f := range fs {
+		log.Println(f)
 	}
 }
 
@@ -50,4 +59,16 @@ func writeFiles(fs gocb.GOCBFiles) error {
 	}
 	err = ioutil.WriteFile(fpath, b.Bytes(), 0777)
 	return err
+}
+func readFiles() (gocb.GOCBFiles, error) {
+	var gocbfiles gocb.GOCBFiles
+	fpath := filepath.Join(src, gobfile)
+	by, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		return gocbfiles, err
+	}
+	b := bytes.NewBuffer(by)
+	dec := gob.NewDecoder(b)
+	err = dec.Decode(&gocbfiles)
+	return gocbfiles, err
 }
